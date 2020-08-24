c5ea57f7f4c855457353c18cd44e6d61efb0c7b7
Peter Hosey <git@boredzo.org>
2020-08-24 12:02:49 -0700

    Refactoring and adding SRT support.

--- extract-webvtt-from-granicus.py
+++ extract-webvtt-from-granicus.py
@@ -10,9 +10,10 @@ import json
 import cgi
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--format', action='store', choices=[ 'webvtt', 'srt' ], default='webvtt')
 opts, args = parser.parse_known_args()
 
-def extract_WebVTT_from_webpage(webpage_URL):
+def extract_captions_from_webpage(webpage_URL):
 	curl = subprocess.Popen([ '/usr/bin/curl', '-L', webpage_URL ], stdout=subprocess.PIPE, stderr=open('/dev/null', 'wb'))
 	'''<!DOCTYPE html>
 <html lang="en">
@@ -29,10 +30,12 @@ def extract_WebVTT_from_webpage(webpage_URL):
 	match = meta_video_exp.search(head)
 	if match:
 		JSON_URL = match.group(1)
-		return extract_WebVTT_from_JSON(JSON_URL)
+		return extract_captions_from_Granicus_JSON(JSON_URL)
 
-def extract_WebVTT_from_JSON(URL_or_path):
-	if URL_or_path.startswith('https:'):
+def extract_captions_from_Granicus_JSON(URL_or_path):
+	if hasattr(URL_or_path, 'read'):
+		JSON = URL_or_path.read()
+	elif URL_or_path.startswith('https:'):
 		if 'modernplayer.swf' in URL_or_path:
 			parsed_SWF_URL = urlparse.urlparse(URL_or_path)
 			query = urlparse.parse_qs(parsed_SWF_URL.query)
@@ -46,9 +49,19 @@ def extract_WebVTT_from_JSON(URL_or_path):
 	else:
 		JSON = open(URL_or_path, 'r').read()
 
-	return convert_Granicus_JSON_to_WebVTT(JSON)
+	decoder = json.JSONDecoder()
+	timestamps = decoder.decode(JSON)
 
-def convert_Granicus_JSON_to_WebVTT(JSON):
+	last_caption = None
+	for each in timestamps[0]:
+		if each['type'] == 'text':
+			if last_caption is not None:
+				last_caption['end_time'] = each['time']
+			last_caption = each
+
+	return timestamps
+
+def convert_captions_to_WebVTT(captions):
 	def convert_seconds_to_timestamp(total_seconds_str):
 		total_seconds = float(total_seconds_str)
 		hours = int(total_seconds / 3600)
@@ -64,17 +77,8 @@ def convert_Granicus_JSON_to_WebVTT(JSON):
 		'''WEBVTT
 '''
 	]
-	decoder = json.JSONDecoder()
-	timestamps = decoder.decode(JSON)
 
-	last_caption = None
-	for each in timestamps[0]:
-		if each['type'] == 'text':
-			if last_caption is not None:
-				last_caption['end_time'] = each['time']
-			last_caption = each
-
-	for each in timestamps[0]:
+	for each in captions[0]:
 		if each['type'] == 'text':
 			start_timestamp = convert_seconds_to_timestamp(each['time'])
 			try:
@@ -90,6 +94,41 @@ def convert_Granicus_JSON_to_WebVTT(JSON):
 
 	return '\n'.join(chunks)
 
+def convert_captions_to_SRT(captions):
+	def convert_seconds_to_timestamp(total_seconds_str):
+		total_seconds = float(total_seconds_str)
+		hours = int(total_seconds / 3600)
+		minutes = int(total_seconds / 60) % 60
+		seconds = total_seconds % 60.0
+		# Sadly %02.3f doesn't Just Work, so we have to do this garbage.
+		seconds_int = int(seconds)
+		seconds_frac = seconds - seconds_int
+		seconds_str = '%02u' % (seconds_int,) + ('%.3f' % (seconds_frac,)).lstrip('0')
+		return ('%02u:%02u:%s' % (hours, minutes, seconds_str)).replace('.', ',')
+	
+	chunks = [
+		''
+	]
+	counter = 0
+
+	for each in captions[0]:
+		if each['type'] == 'text':
+			start_timestamp = convert_seconds_to_timestamp(each['time'])
+			try:
+				end_timestamp = convert_seconds_to_timestamp(each['end_time'])
+			except KeyError:
+				# The last caption won't have an end time, since the end time is the start time of the caption after it. Make up a five-second duration for the last caption.
+				fake_end_time = str(float(each['time']) + 5.0)
+				end_timestamp = convert_seconds_to_timestamp(fake_end_time)
+			text = each['text']
+			counter = counter + 1
+			chunks.append('''%u
+%s --> %s
+%s
+''' % (counter, start_timestamp, end_timestamp, cgi.escape(text)))
+
+	return '\n'.join(chunks)
+
 try:
 	(URL_or_path,) = args
 	use_stdin = False
@@ -97,8 +136,13 @@ except ValueError:
 	use_stdin = True
 
 if use_stdin:
-	sys.stdout.write(convert_Granicus_JSON_to_WebVTT(sys.stdin.read()))
+	captions = extract_captions_from_Granicus_JSON(sys.stdin)
 elif URL_or_path.startswith('https:'):
-	sys.stdout.write(extract_WebVTT_from_webpage(URL_or_path))
+	captions = extract_captions_from_webpage(URL_or_path)
 else:
-	sys.stdout.write(extract_WebVTT_from_JSON(URL_or_path))
+	captions = extract_captions_from_Granicus_JSON(URL_or_path)
+
+if opts.format == 'srt':
+	sys.stdout.write(convert_captions_to_SRT(captions))
+else:
+	sys.stdout.write(convert_captions_to_WebVTT(captions))
